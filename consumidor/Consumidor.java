7f7aa14b7015cd3ab103158115a486d015361bac
Jovitor-jva <joaovitorlima1936@hotmail.com>
2023-10-10 09:57:49 -0300

    some refactoring

--- consumidor/Consumidor.java
+++ consumidor/Consumidor.java
@@ -4,18 +4,17 @@ import armazem.Armazem;
 
 public class Consumidor extends Thread {
     private Armazem armazem;
-    private int quantidadeItensMaximoParaConsumo = 20;
+    private int quantidadeDeItensQueOConsumidorVaiConsumir = 20;
 
     public Consumidor(Armazem armazem) {
         this.armazem = armazem;
     }
 
     public void run() {
-        for (int i = 1; i <= quantidadeItensMaximoParaConsumo; i++) {
+        for (int i = 1; i <= quantidadeDeItensQueOConsumidorVaiConsumir; i++) {
             try {
                 armazem.getConsumoDoArmazem();
             } catch (Exception e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
--- produtor/Produtor.java
+++ produtor/Produtor.java
@@ -5,14 +5,14 @@ import armazem.Armazem;
 public class Produtor extends Thread {
     private Armazem armazem;
     private int itensProduzidos = 0;
-    private int capacidadeDeProducaoDoArmazem = 20;
+    private int quantidadeDeItensQueOProdutorVaiConsumir = 20;
 
     public Produtor(Armazem armazem) {
         this.armazem = armazem;
     }
 
     public void run() {
-        for (int i = 1; i <= capacidadeDeProducaoDoArmazem; i++) {
+        for (int i = 1; i <= quantidadeDeItensQueOProdutorVaiConsumir; i++) {
             armazem.getProducaoDoArmazem(i);
             itensProduzidos++;
         }
